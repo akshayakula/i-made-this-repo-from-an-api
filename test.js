62ff1b6d58a7e8a734d30c140b306b55c99a65b1
Akshay Akula <47546030+akshayakula@users.noreply.github.com>
2022-10-29 22:35:51 -0400

    Added by Jira

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
 const sample1 = () => {
 // Reflexx Bug - JIR-10 - https://akshayakula.atlassian.net/browse/JIR-10
-  console.log(r)
+  console.log('hi');
   console.log('hi')
   const hi = 'test'
   const sample5 = function () {
     console.log('hi')
   }
 }
+
 
 const sample2 = function () {
   console.log('hi')
