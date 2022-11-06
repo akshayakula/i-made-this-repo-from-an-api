366cee24dfb276846d7ca00a6661382b3e5bbd60
Akshay Akula <47546030+akshayakula@users.noreply.github.com>
2022-11-06 17:17:43 -0500

    Added by Jira

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
 const sample = function() {
+
+	num = num + value
+	if (num !== 6) {
+		throw new Exception("Value should be 6");
+	}
+}mple = function() {
   var num = 5
 // Reflexx Bug - JIR-5 - https://akshayakula.atlassian.net/browse/JIR-5
 	num = num + value
